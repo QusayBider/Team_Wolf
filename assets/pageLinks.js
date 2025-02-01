3d4490b8a1c776a587dd5c6ebb59c443cf703bfc
QusayBider <QusayBider@gmail.com>
2025-02-01 14:32:01 +0200

    Up

--- assets/pageLinks.js
+++ assets/pageLinks.js
@@ -1,5 +1,6 @@
 let VideoTransition = document.getElementById("VedioTransition");
 let Links=document.getElementById("Links");
+let liveKick=document.getElementById("liveKick");
 function removeTranstion() {
  
     interval = setInterval(() => {
@@ -15,3 +16,10 @@ function removeTranstion() {
     }, 2000); 
   }
   put_Links_home()
+  function put_Live_home() {
+ 
+    interval = setInterval(() => {
+      liveKick.classList.remove("hiddenFromScreen");
+    }, 2000); 
+  }
+  put_Live_home()
